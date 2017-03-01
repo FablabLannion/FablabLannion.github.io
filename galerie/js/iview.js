efcb72a96939e61ca564e3171326178526e241fb
yannlossouarn <yann@lossouarn.net>
2017-03-01 11:39:51 +0100

    message log

--- galerie/js/iview.js
+++ galerie/js/iview.js
@@ -22,6 +22,7 @@
 			//Get slider
 			iv.slider = $('.iviewSlider', iv.sliderContent);
 			iv.slider.css('position', 'relative');
+			console.log("avant de setter le background-size=cover");
 			iv.slider.css('background-size', 'cover');
 
 			//Necessary variables.
